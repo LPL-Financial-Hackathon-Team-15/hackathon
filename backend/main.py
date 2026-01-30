8cc26ae933cc3acfd9bd9ba8a6b359a64deb03bc
caileanmatthews <matthewsmcailean@gmail.com>
2026-01-30 15:03:58 -0500

    Try webhook
    ci/cd pipeline

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import subprocess
 
 app = FastAPI()
+
+@app.post("/webhook")
+async def github_webhook(request: Request):
+    payload = await request.json()
+
+    # Optional: check branch
+    if payload["ref"] == "refs/heads/main":
+        subprocess.run(["git", "pull"], cwd="/home/ec2-user/")
+
+    return {"status": "ok"}
 
 @app.get("/")
 def read_root():
