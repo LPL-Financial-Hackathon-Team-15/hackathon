0ed95c8dbaa6fc79c1e96264f29d859848e09249
caileanmatthews <matthewsmcailean@gmail.com>
2026-01-30 15:19:53 -0500

    Updated host

--- frontend-project/vite.config.js
+++ frontend-project/vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["ec2-3-142-36-77.us-east-2.compute.amazonaws.com"]
+  }
 })
